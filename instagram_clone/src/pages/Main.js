ece867ae2d445c4a78272ad3bb3c2adeb3cfa0c5
박준영 <skyzuni@nate.com>
2021-07-17 17:10:31 +0900

    #6 Post 컴포넌트 통합 필요

--- instagram_clone/src/pages/Main.js
+++ instagram_clone/src/pages/Main.js
@@ -4,10 +4,19 @@ import Post from "../components/Post";
 import Header from "../components/Header";
 
 const Main = (props) => {
+
+  const postList = [
+    {name: "닉네임1", contents: "테스트용 글내용1 테스트용 글내용1 테스트용 글내용1", time: "2021-07-17"},
+    {name: "닉네임2", contents: "테스트용 글내용2 테스트용 글내용2", time: "2021-07-17"},
+    {name: "닉네임3", contents: "테스트용 글내용3 테스트용 글내용3 테스트용 글내용3 테스트용 글내용3 테스트용 글내용3", time: "2021-07-17"}
+  ]
+
   return(
     <React.Fragment>
       <Header></Header>
-      <Post></Post>
+      {postList.map((_, idx) =>{
+        return(<Post key={idx} {..._}></Post>)
+      })}
     </React.Fragment>
   );
 }
